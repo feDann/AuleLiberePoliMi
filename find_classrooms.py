b5319fdfbedeeb10787826dd0f7255e586acd127
feDann <daniele99@ferrazzo.it>
2021-05-30 17:27:58 +0200

    Implement free_classroom function

--- find_classrooms.py
+++ find_classrooms.py
@@ -9,11 +9,14 @@ ROOM = 'dove'
 LECTURE = 'slot'
 
 
-# Return a dict with all the info about the classrooms for the chosen day
+# Return a dict with all the info about the classrooms for the chosen day , 
+# the function makes a get requests to the URL and then 
+# build a dict with the classes information stored on the html table (the code may not be perfect 🥲)
+
 def find_classrooms(location , day , month , year):
     info = {} 
     buildingName = '-' #defaul value for building
-    info[buildingName] = {}
+    info[buildingName] = {} #first initialization due to table format
 
     params = {'csic': location , 'categoria' : 'tutte', 'tipologia' : 'tutte', 'giorno_day' : day , 'giorno_month' : month, 'giorno_year' : year , 'jaf_giorno_date_format' : 'dd%2FMM%2Fyyyy'  , 'evn_visualizza' : ''}
     r = requests.get(URL , params= params)
@@ -28,24 +31,27 @@ def find_classrooms(location , day , month , year):
             if BUILDING in tds[0].attrs['class']:
                 buildingName = tds[0].string
                 buildingName = re.search('(Edificio.*)' , buildingName).group(1) #take only the building name
-                info[buildingName] = {}
+                if buildingName not in info:
+                    info[buildingName] = {}
         else:
             room = ''
             time = 7.75
             for td in tds:
                 if ROOM in td.attrs['class']:
                     room = td.find('a').string
-                    info[buildingName][room] = {}
-                    info[buildingName][room]['lessons'] = []
+                    
+                    if room not in info[buildingName]:
+                        info[buildingName][room] = {}
+                        info[buildingName][room]['lessons'] = []
+
                 elif LECTURE in td.attrs['class'] and room != '':
                     duration = int(td.attrs['colspan'])
                     lesson_name = td.find('a').string
-                    slot = {}
-                    slot['from'] = time
-                    time += duration/4
-                    slot['to'] = time
                     lesson = {}
-                    lesson[lesson_name] = slot                    
+                    lesson['name'] = lesson_name
+                    lesson['from'] = time
+                    time += duration/4
+                    lesson['to'] = time
                     info[buildingName][room]['lessons'].append(lesson)
                 else:
                     time += 0.25
---
+++ free_classroom.py
@@ -0,0 +1,34 @@
+from find_classrooms import find_classrooms
+import datetime
+from pprint import pprint
+
+def _is_room_free(lessons , starting_time , ending_time):
+    if len(lessons) == 0:
+        return True
+    for lesson in lessons:
+        start = float(lesson['from'])
+        end = float(lesson['to'])
+        if starting_time < start and start < ending_time:
+            return False
+        if start < starting_time and end > starting_time:
+            return False
+    return True
+        
+
+def find_free_room(starting_time , ending_time , location , day , month , year):
+    free_rooms = {}
+    infos = find_classrooms(location , day , month , year)
+    for building in infos:
+        for room in infos[building]:
+            lessons = infos[building][room]['lessons']
+            if _is_room_free(lessons , starting_time , ending_time):
+                if building not in free_rooms:
+                    free_rooms[building] = []
+                free_rooms[building].append(room)
+    
+    pprint(free_rooms)
+
+
+if __name__ == "__main__":
+    now = datetime.datetime.now()
+    find_free_room(14 , 18 , 'MIA', 31 , 5 , 2021)
