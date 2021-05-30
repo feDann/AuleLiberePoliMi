204da6f4f0a7f6b7400e600d9762db768a845d31
feDann <daniele99@ferrazzo.it>
2021-05-30 16:20:02 +0200

    little code refactor

--- find_classrooms.py
+++ find_classrooms.py
@@ -4,33 +4,40 @@ import json
 import re
 
 URL = "https://www7.ceda.polimi.it/spazi/spazi/controller/OccupazioniGiornoEsatto.do"
+BUILDING = 'innerEdificio'
+ROOM = 'dove'
+LECTURE = 'slot'
 
 
 # Return a dict with all the info about the classrooms for the chosen day
 def find_classrooms(location , day , month , year):
-    info = {'-' : {}}
-    buildingName = '-'
+    info = {} 
+    buildingName = '-' #defaul value for building
+    info[buildingName] = {}
+
     params = {'csic': location , 'categoria' : 'tutte', 'tipologia' : 'tutte', 'giorno_day' : day , 'giorno_month' : month, 'giorno_year' : year , 'jaf_giorno_date_format' : 'dd%2FMM%2Fyyyy'  , 'evn_visualizza' : ''}
     r = requests.get(URL , params= params)
+    
     soup = BeautifulSoup(r.text, 'html.parser')
     tableContainer = soup.find("div", {"id": "tableContainer"})
-    tableRows = tableContainer.find_all('tr')[3:]
+    tableRows = tableContainer.find_all('tr')[3:] #remove first three headers
+    
     for row in tableRows:
         tds = row.find_all('td')
         if 'class' not in row.attrs:
-            if 'innerEdificio' in tds[0].attrs['class']:
+            if BUILDING in tds[0].attrs['class']:
                 buildingName = tds[0].string
-                buildingName = re.search('(Edificio.*)' , buildingName).group(1)
+                buildingName = re.search('(Edificio.*)' , buildingName).group(1) #take only the building name
                 info[buildingName] = {}
         else:
             room = ''
             time = 7.75
             for td in tds:
-                if 'dove' in td.attrs['class']:
+                if ROOM in td.attrs['class']:
                     room = td.find('a').string
                     info[buildingName][room] = {}
                     info[buildingName][room]['lessons'] = []
-                elif 'slot' in td.attrs['class'] and room != '':
+                elif LECTURE in td.attrs['class'] and room != '':
                     duration = int(td.attrs['colspan'])
                     lesson_name = td.find('a').string
                     slot = {}
